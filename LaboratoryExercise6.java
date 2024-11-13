81116bcdc09e237b36be736f4f3da47f7365a7aa
JenCreates <jenniferflores142005@gmail.com>
2024-11-13 09:45:14 +0800

    Add files via upload

---
+++ LaboratoryExercise6.java
@@ -0,0 +1,25 @@
+package laboratory.exercise.pkg6;
+
+public class LaboratoryExercise6 {
+ 
+    public static void main(String[] args) {
+     int iNumber = 3;
+     
+     if (iNumber >=0){
+     if (iNumber==0){        
+         System.out.println("first string");
+         
+       
+         System.out.println("second string");
+          }
+     
+     else {
+        }
+          System.out.println("third string");
+         
+        
+        
+        
+    }
+    
+}
